296e77a7593f092bf8d3bc3d333496d2829b7cad
Arya Anish <anishkumarsarmi@gmail.com>
2021-09-09 15:01:51 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const client = new Client();
 mongoose.connect('mongodb://localhost:27017/codeoftheday');
 
 const channelSchema = new mongoose.Schema({
-  id: String,
+  channelId: String,
   category: String,
 });
 
@@ -19,36 +19,49 @@ client.on('message', (message) => {
   if (message.content.startsWith(PREFIX)) {
     const [cmd, value] = message.content.replace(PREFIX, '').trim().split(' ');
     if (cmd === 'r') {
-      if (!value) {
-        message.reply(
-          'Please give a question category, format should be ```!code r <category (javascript or python)>```'
-        );
-      } else {
-        if (
-          value.toLocaleLowerCase() === 'javascript' ||
-          value.toLocaleLowerCase() === 'python'
-        ) {
-          const channelId = message.channel.id;
-          const channel = new Channel({
-            id: channelId,
-            category: value,
-          });
-          channel.save((err) => {
-            if (err) {
-              console.log(err);
-            } else {
-            }
-          });
+      Channel.findOne({ channelId: message.channel.id }, (err, result) => {
+        if (err) {
+          console.log(err);
         } else {
-          message.reply(
-            'Please give a valid question category, format should be ```!code r <category (javascript or python)>```'
-          );
+          if (result) {
+            message.reply('Channel already registered');
+          } else {
+            if (!value) {
+              message.reply(
+                'Please give a question category, format should be ```!code r <category (javascript or python)>```'
+              );
+            } else {
+              if (
+                value.toLocaleLowerCase() === 'javascript' ||
+                value.toLocaleLowerCase() === 'python'
+              ) {
+                const channelId = message.channel.id;
+                const channel = new Channel({
+                  channelId: channelId,
+                  category: value,
+                });
+                channel.save((err) => {
+                  if (err) {
+                    console.log(err);
+                  } else {
+                    message.reply(
+                      'Done... Now, the bot will send a question every 10 minutes'
+                    );
+                  }
+                });
+              } else {
+                message.reply(
+                  'Please give a valid question category, format should be ```!code r <category (javascript or python)>```'
+                );
+              }
+            }
+          }
         }
-      }
+      });
     } else {
       message.reply(`Available commands 
-1. !code r
-2. !code q
+1. !code r - Register this channel to get questions
+2. !code q - Get an instant question
       `);
     }
   }
